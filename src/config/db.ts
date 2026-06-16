4d4400c8b2848a75d096730c19bde5d9f358dd31
Gabriel Duah <gabrielagyemanduah@gmail.com>
2026-06-16 14:50:24 +0000

    feature/communication admin is added

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import dns from 'node:dns/promises';
 import logger from '../utils/logger';
 
 const connectDB = async (): Promise<void> => {
@@ -9,6 +10,7 @@ const connectDB = async (): Promise<void> => {
   }
 
   try {
+    dns.setServers(['8.8.8.8', '1.1.1.1']);
     await mongoose.connect(mongoUri);
     logger.info('MongoDB connected');
   } catch (error) {
